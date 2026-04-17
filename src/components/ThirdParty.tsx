cd0060b8bf7f9f557f233129d9b72ae47fdfcf9b
lisanshidqifarizan <lisanshidqifarizan@gmail.com>
2026-04-18 06:07:58 +0700

    changing blog & portfolio position

--- src/components/ThirdParty.tsx
+++ src/components/ThirdParty.tsx
@@ -1,31 +1,34 @@
-// * CookieBanner.tsx
 "use client";
+// * CookieBanner.tsx
 import Script from "next/script";
 
 export const CookieBanner = () => {
-    return (
-        <Script
-            id="Cookiebot"
-            src="https:/consent.cookiebot.com/uc.js"
-            data-cbid="8be92394-9779-47d9-bf86-80466ad9fec0"
-            strategy="afterInteractive"
-        />
-    );
-}
+	return (
+		<Script
+			id="Cookiebot"
+			src="https:/consent.cookiebot.com/uc.js"
+			data-cbid="8be92394-9779-47d9-bf86-80466ad9fec0"
+			strategy="afterInteractive"
+		/>
+	);
+};
 
-/ * GoogleAnalytic.tsx
+// * GoogleAnalytic.tsx
 export const GoogleAnalytic = () => {
-    return (
-        <>
-            <Script async src="https:/www.googletagmanager.com/gtag/js?id=G-KN2QD3WWYK" />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
+	return (
+		<>
+			<Script
+				async
+				src="https:/www.googletagmanager.com/gtag/js?id=G-KN2QD3WWYK"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-KN2QD3WWYK');
         `}
-            </Script>
-        </>
-    );
-}
+			</Script>
+		</>
+	);
+};
