29f91601d2d71fb72ab0e96ea55b0e4d0bf19259
lisanshidqifarizan <lisanshidqifarizan@gmail.com>
2026-04-18 21:39:28 +0700

    evaluate my sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,6 +11,58 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			lastModified: new Date(),
 		},
 		{
+			url: `${baseUrl}/login`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/register`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/disclaimer`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/faq`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/about`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/profile`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/contact`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/about`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/tags`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/privacy-policy`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/faq`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/portfolio/`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/portfolio/about`,
+			lastModified: new Date(),
+		},
+		{
 			url: `${baseUrl}/portfolio/projects`,
 			lastModified: new Date(),
 		},
